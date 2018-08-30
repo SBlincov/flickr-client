9ec0aae43f296239bfdcd671201a873412e4b055
Blintsov Sergey <sblincov@blintsovs-macbook-pro.local>
2018-08-30 18:14:59 +0300

    Initial Commit

---
+++ flickr-client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flickr-client
+//
+//  Created by Blintsov Sergey on 30/08/2018.
+//  Copyright © 2018 Blintsov Sergey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
