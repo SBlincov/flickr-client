f76ae6dd0eb341a9c282eb49ddaa5269291d0b54
Sergey Blintsov <sblin180@gmail.com>
2018-09-05 01:11:30 +0300

    First screen released

--- flickr-client/ViewController.swift
+++ flickr-client/ViewController.swift
@@ -111,8 +111,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
                    shouldChangeCharactersIn range: NSRange,
                    replacementString string: String)
         -> Bool {
- 
-        if string.characters.count == 0 {
+        if string.count == 0 {
             return true
         }
         
@@ -251,13 +250,13 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             guard let data = data, error == nil else {
-                print("Error when run request = \(error)")
+                print("Error when run request = \(String(describing: error))")
                 return
             }
             
             if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
                 print("statusCode not equal 200, statusCode is \(httpStatus.statusCode)")
-                print("Response = \(response)")
+                print("Response = \(String(describing: response))")
             }
             
             guard let dict = (try? JSONSerialization.jsonObject(with: data, options: [])) as? NSDictionary,
@@ -278,13 +277,13 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             guard let data = data, error == nil else {
-                print("Error when run request = \(error)")
+                print("Error when run request = \(String(describing: error))")
                 return
             }
             
             if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
                 print("statusCode not equal 200, statusCode is \(httpStatus.statusCode)")
-                print("Response = \(response)")
+                print("Response = \(String(describing: response))")
             }
             
             guard let dict = (try? JSONSerialization.jsonObject(with: data, options: [])) as? NSDictionary,
